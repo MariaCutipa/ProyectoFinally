6fc8e2ac2bba46525455f2bc9160c44f3a85c1ae
MariaCutipa <129446025+MariaCutipa@users.noreply.github.com>
2024-11-22 11:53:19 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const multer = require('multer');
-const path = require('path');
+const path = require('path
+require('dotenv').config(); 
 
 const app = express();
 app.use(bodyParser.json());
